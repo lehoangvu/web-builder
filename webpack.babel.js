c495ab0e9190281298cac9dbccf72513c0541d46
Vũ Lê <vu.le@tiki.vn>
2017-10-30 17:26:01 +0700

    first commit

---
+++ webpack.babel.js
@@ -0,0 +1,43 @@
+import path from 'path';
+import webpack from 'webpack';
+import Package from './package.json';
+
+
+const config = {
+	cache: true,
+	entry:  {
+		app: './src/app.js',
+	},
+	output: {
+		filename: '[name].js',
+		path:     path.join(__dirname, 'dist')
+	},
+	plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'APP_ENV': JSON.stringify('development')
+            }
+        }),
+		new webpack.ExtendedAPIPlugin()
+	],
+	node: {
+		fs: "empty"
+	},
+	module: {
+		rules: [
+		{
+			exclude: /node_modules/,
+			use:  ['babel-loader'],
+			test:    /\.js?$/
+		}
+		]
+	},
+	resolve: {
+		modules: [
+			path.join(__dirname, "src"),
+			"node_modules"
+		]
+	}
+};
+
+export default config;
